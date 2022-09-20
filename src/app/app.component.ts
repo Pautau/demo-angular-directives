894436ca3ee4f7d768c4a966c4fac71f3f31d519
Florian <caron.florian7620@gmail.com>
2022-09-20 16:14:57 +0200

    add directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-angular-4';
+  title = 'Les directives';
+  informations: string[] = [
+    "Les chats sont les bests",
+    'Les chiens ça pue',
+    "Bouuuh les serpents"
+  ];
+  classes = {
+    'red': this.informations.length <=2,
+    'green': this.informations.length >=2
+  }
 }
